8ff3e925b382e5f6f222849bf792312d0a70d21e
PedroDMonteiro <38916855+PedroDMonteiro@users.noreply.github.com>
2026-03-16 13:32:11 -0300

    improve utils
    encapsulamento em classe
    nova função is_iterable

--- utils/utils.py
+++ utils/utils.py
@@ -3,14 +3,21 @@ from discord import File
 import requests
 import aiohttp
 
+class Utils:
+    async def get_file_from_url(url: str,name: str)-> File:
+        async with aiohttp.ClientSession().get(url) as response:
+            buffer = BytesIO(response.content)
+            return File(fp=buffer,filename=name)
 
-async def get_file_from_url(url: str,name: str)-> File:
-    async with aiohttp.ClientSession().get(url) as response:
-        buffer = BytesIO(response.content)
-        return File(fp=buffer,filename=name)
+    async def save_url(url: str, path: str) -> bool:
+        async with aiohttp.ClientSession().get(url) as response:
+            buffer = BytesIO(response.content)
+            with open(path, "wb") as binary_file:
+                binary_file.write(buffer.getvalue())
 
-async def save_url(url: str, path: str) -> bool:
-    async with aiohttp.ClientSession().get(url) as response:
-        buffer = BytesIO(response.content)
-        with open(path, "wb") as binary_file:
-            binary_file.write(buffer.getvalue())
+    def is_iterable(obj: any) -> bool:
+        try:
+            iter(obj)
+            return True
+        except Exception as err:
+            return False
