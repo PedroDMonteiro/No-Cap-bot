7d02463ed0aaa5b7146ababcaa9db75558feb3ec
PedroDMonteiro <38916855+PedroDMonteiro@users.noreply.github.com>
2026-03-23 21:43:20 -0300

    (secret) improve

--- cogs/secret/main.py
+++ cogs/secret/main.py
@@ -13,19 +13,21 @@ class Cog_Secret(Cog, name = "Secret"):
     def __init__(self, bot: MyBot):
         self.bot = bot
 
+    async def cog_load(self):
+        self.channel = await self.bot.fetch_channel(1356712387163586801)
+
     @commands.Cog.listener()
     async def on_message(self, message: Message):
         if message.author.id != self.bot.user.id:
             if isinstance(message.channel, discord.channel.DMChannel):
-                channel = self.bot.get_channel(1356712387163586801)
                 msg = f"## {message.author} Send \n-# ID:{message.author.id}\n" + message.content
-                await channel.send(msg, files=[await attch.to_file() for attch in message.
-                attachments])
+                await self.channel.send(msg, files=[
+                    await attch.to_file() for attch in message.attachments])
 
     @commands.Cog.listener()
     async def on_message_edit(self, before: discord.Message, after: discord.Message):
         if after.author.id != self.bot.user.id:
             if isinstance(after.channel, discord.channel.DMChannel):
-                channel = self.bot.get_channel(1356712387163586801)
                 after.content = f"## {after.author} Send \n-# ID:{after.author.id}\n" + after.content
-                await channel.send(after.content, files=[await attch.to_file() for attch in after.attachments])
+                await self.channel.send(after.content, files=[
+                    await attch.to_file() for attch in after.attachments])
